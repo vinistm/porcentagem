1a51e2ee29ae2db455ee71567b7ffda486427120
Vinicius Martins <vinicius.stmartins@gmail.com>
2023-09-13 12:20:54 -0300

    ajuste

--- script.js
+++ script.js
@@ -16,7 +16,6 @@ const itemWeights = {
   "Over Head Squat": [],
 };
 
-// Carregar os pesos salvos do Local Storage
 for (const item in itemWeights) {
   if (localStorage.getItem(item)) {
     itemWeights[item] = JSON.parse(localStorage.getItem(item));
@@ -44,8 +43,8 @@ function closeModal() {
 function selectItem(item) {
   selectedItem = item;
   openModal(item);
-  weights = itemWeights[item]; // Carregar os pesos salvos para o item selecionado
-  updateModalPercentageList(); // Atualizar a lista de porcentagens no modal
+  weights = itemWeights[item]; 
+  updateModalPercentageList(); 
 }
 
 function addWeightModal() {
@@ -83,6 +82,11 @@ function updateModalPercentageList() {
       weights[weights.length - 1],
       percentage
     )} Lbs`;
+
+    const showPlatesButton = document.createElement("span");
+    showPlatesButton.innerHTML = `<button id='plate' onclick="showPlates(${percentage})"> </button>`;
+    listItem.appendChild(showPlatesButton);
+
     percentageList.appendChild(listItem);
   }
 }
@@ -107,6 +111,68 @@ function calculateModalPercentage() {
     )} Lbs`;
   }
 }
+
+function showPlates(percentage) {
+  const platesModal = document.getElementById("platesModal");
+  platesModal.style.display = "block";
+
+  const platesList = document.getElementById("plates-list");
+  platesList.innerHTML = "";
+
+  const totalWeight = calculatePercentageValue(
+    weights[weights.length - 1],
+    percentage
+  );
+
+  const availablePlates = [45, 35, 25, 10, 5, 2.5]; 
+
+  const barDescriptions = {
+    male: "Barra Masculina",
+    female: "Barra Feminina",
+  };
+
+  const plates = {};
+
+  for (const barType in barDescriptions) {
+    const barWeight = barType === "female" ? 35 : 45; 
+
+    let remainingWeight = totalWeight - barWeight;
+    const barPlates = [];
+
+    
+    if (remainingWeight > 0) {
+      remainingWeight /= 2;
+
+      for (const plate of availablePlates) {
+        const plateCount = Math.floor(remainingWeight / plate);
+        if (plateCount > 0) {
+          const plateText = `${plateCount}x ${plate} lbs`;
+          barPlates.push(plateText);
+          remainingWeight -= plateCount * plate;
+        }
+      }
+    }
+
+    plates[barType] = {
+      description: barDescriptions[barType],
+      weight: barWeight,
+      plates: barPlates,
+    };
+  }
+
+
+  for (const barType in plates) {
+    const bar = plates[barType];
+    const listItem = document.createElement("li");
+    const platesText = bar.plates.map(plate => `+ ${plate} lbs`).join("<br>");
+    listItem.innerHTML = `${bar.description}<br>${platesText}`;
+    platesList.appendChild(listItem);
+  }}
+function closePlatesModal() {
+  const platesModal = document.getElementById("platesModal");
+  platesModal.style.display = "none";
+}
+
 const btnLpo = document.getElementById("btn-Lpo");
 btnLpo.addEventListener("click", () => {
   window.location.href = "Lpo.html";
